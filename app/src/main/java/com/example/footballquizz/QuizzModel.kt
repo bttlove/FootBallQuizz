3a8f6dc6463883265376749d2999f38930dcb537
tienthinh19 <thinhbui1907@gmail.com>
2024-10-22 12:59:48 +0700

    fix manager

--- app/src/main/java/com/example/footballquizz/QuizzModel.kt
+++ app/src/main/java/com/example/footballquizz/QuizzModel.kt
@@ -2,26 +2,26 @@ package com.example.footballquizz
 
 data class QuizzModel(
 
-  val Name: String = "",
+  val  name: String = "",
   val yearOfBirth: Int = 0,
   val club: String = "",
   val imageUrl: String = "",
   var id: String? = null
 ){
 
-
+/*
   fun getEasyQuestion(): String {
          return "Tên cầu thủ trong ảnh là gì?"
      }
      fun getMediumQuestion(): String {
-         return "Cầu thủ $Name hiện đang chơi cho câu lạc bộ nào?"
+         return "Cầu thủ $name hiện đang chơi cho câu lạc bộ nào?"
      }
      fun getHardQuestion(): String {
-         return "Cầu thủ $Name sinh vào năm nào?"
-     }
+         return "Cầu thủ $name sinh vào năm nào?"
+     }*/
      fun checkAnswer(answer: String, level: String): Boolean {
          return when (level.toLowerCase()) {
-             "easy" -> answer.equals(Name, ignoreCase = true)
+             "easy" -> answer.equals(name, ignoreCase = true)
              "medium" -> answer.equals(club, ignoreCase = true)
              "hard" -> answer == yearOfBirth.toString()
              else -> false
